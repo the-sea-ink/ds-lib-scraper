3f4587604367b709aaa08baa6815403195092bee
xenia-melman <xenia.melman@gmail.com>
2023-01-12 12:26:49 +0100

    added args, adjusted project settings

--- ds_scraper/ds_scraper/postprocessing.py
+++ ds_scraper/ds_scraper/postprocessing.py
@@ -1,10 +1,23 @@
-import os
+import argparse
 
-#os.system('(head -n 1 spiders/items.csv && tail -n+2 spiders/items.csv | sort) > spiders/pandas_kb.csv')
+parser = argparse.ArgumentParser(description='Applies postprocessing to raw scraped data. Currently only adds '
+                                             'an index column.')
 
-with open('spiders/items.csv') as infile, open('spiders/pandas_kb.csv', 'w') as outfile:
-    for idx, line in enumerate(infile):
-        if idx == 0:
-            outfile.write(f'index,{line}')
-        else:
-            outfile.write(f'{idx},{line}')
+parser.add_argument('-i', '--input_file', help='Input file', required=True)
+parser.add_argument('-o', '--output_file', help='Output file', required=True)
+args = parser.parse_args()
+
+
+def enum():
+    infile_path = args.input_file
+    outfile_path = args.output_file
+
+    with open(infile_path) as infile, open(outfile_path, 'w') as outfile:
+        for idx, line in enumerate(infile):
+            if idx == 0:
+                outfile.write(f'index,{line}')
+            else:
+                outfile.write(f'{idx},{line}')
+
+
+enum()
